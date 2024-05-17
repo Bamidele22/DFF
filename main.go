ead2786dd8a305e01767e9dd7ec90c68af1e07ae
user.name: "bamideledeji001@gmial.com <bamideledeji001@gmail.com>
2024-05-17 13:07:47 +0100

    Added bootstrap logic

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "duplicate-finder",
+	Short: "Find and remove duplicate files in a directory.",
+	Long: `Duplicate Finder is a command-line utility that scans directories for duplicate files and provides an option to delete them.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Please specify a directory to scan.")
+			return
+		}
+
+		directory := args[0]
+		findDuplicates(directory)
+	},
+}
+
+func findDuplicates(dir string) {
+	// Implementation goes here
+}
+
+func main() {
+	cobra.CheckErr(rootCmd.Execute())
+}
