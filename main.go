25bdf2987865c8028cf98a362620abe258e70c7e
user.name: "bamideledeji001@gmial.com <bamideledeji001@gmail.com>
2024-05-20 12:10:20 +0100

    Added some logic i don't understand yet

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
-
+	"bufio"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 	"github.com/spf13/cobra"
 )
 
@@ -22,7 +30,54 @@ var rootCmd = &cobra.Command{
 }
 
 func findDuplicates(dir string) {
-	// Implementation goes here
+	// Implementation 
+	var wg sync.WaitGroup
+	filesMap := make(map[string][]string)
+
+	// Traverse the directory
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err!= nil {
+			return err
+		}
+
+		// Calculate MD5 hash for file content
+		hasher := md5.New()
+		file, err := os.Open(path)
+		if err!= nil {
+			return err
+		}
+		defer file.Close()
+
+		if _, err := io.Copy(hasher, file); err!= nil {
+			return err
+		}
+
+		contentHash := hex.EncodeToString(hasher.Sum(nil))
+		filesMap[contentHash] = append(filesMap[contentHash], path)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			checkDuplicates(contentHash, filesMap)
+		}()
+
+		return nil
+	})
+
+	if err!= nil {
+		fmt.Println(err)
+		return
+	}
+
+	wg.Wait()
+}
+
+func checkDuplicates(contentHash string, filesMap map[string][]string) {
+	if len(filesMap[contentHash]) > 1 {
+		for _, filePath := range filesMap[contentHash] {
+			fmt.Printf("Duplicate found: %s\n", filePath)
+		}
+	}
 
 }
 
